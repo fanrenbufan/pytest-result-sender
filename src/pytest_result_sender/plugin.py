809dd91627c436e617a83b21b474a3389b116a15
fanrenhl <fanrenhl@163.com>
2026-03-10 13:47:06 +0800

    Signed-off-by: fanrenhl <fanrenhl@163.com>

---
+++ src/pytest_result_sender/plugin.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+def pytest_configure(config):
+    # 在配置文件加载完毕之后执行，在所有测试用例执行之前执行
+    print(f"{datetime.now()} - {__name__}pytest开始执行")
+
+def pytest_unconfigure(config):
+    # 在配置卸载完毕之后执行，在所有测试用例执行之后执行
+    print(f"{datetime.now()}pytest结束执行")
