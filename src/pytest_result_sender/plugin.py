36a4fd44b1c3e91b8a60a8d7f83b8967cee79e17
fanrenhl <fanrenhl@163.com>
2026-03-10 14:53:44 +0800

    使用black、isort、flake8三个工具代

--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
+print("我也来试试")
 
 
-print('我也来试试')
 def pytest_configure(config):
     # 在配置文件加载完毕之后执行，在所有测试用例执行之前执行
     print(f"{datetime.now()} - {__name__}pytest开始执行")
+
 
 def pytest_unconfigure(config):
     # 在配置卸载完毕之后执行，在所有测试用例执行之后执行
